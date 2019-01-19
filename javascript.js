7aa1d9043617d6dcde7fe708805723d77bd02b94
Jeff Levine <jlevine_84@yahoo.com>
2019-01-19 13:59:12 -0500

    Updates to styling and js

--- javascript.js
+++ javascript.js
@@ -3,29 +3,35 @@ $(document).ready(function() {
         var beginDate = $("#startYear").val();
         var endDate = $("#endYear").val();
         var searched = $("#searchTerm").val();
-        var recordsDisplayed= $("#numRecordBox").val();
+        var recordsDisplayed = $("#numRecordBox").val();
         var queryURL="https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/search/v2/articlesearch.json?bebin_date="+ beginDate+ "&end_date="+ endDate+"&q="+ searched+ "&api-key=hvH1MpXQxknB9RAI34sKfjYOv0WiWi6I";
-
-
+        
+        // if(beginDate.length==4){
+        //     beginDate= beginDate+ "0101";
+        // };
+        // if(endDate.length==4){
+        //     endDate= endDate+"0101";
+        // };
         $.ajax({
             url: queryURL,
             method: "GET"
         })
         .then(function(response){
             console.log(response);
-            if(beginDate.length==4){
-                beginDate= beginDate+ "0101";
-            };
-            if(endDate.length==4){
-                endDate= endDate+"0101";
-            };
+        
+        for (var i = 0; i < recordsDisplayed; i++) {
+            var articleDiv = $("<div>").addClass("article");
+            var headlineel = $("<h2>");
+            headlineel.text(response.response.doc[i].headline.main);
+            var abstractel = $("<p>");
+            abstractel.text(response.response.doc[i].abstract);
+            var linkel = $("<a>");
+            linkel.attr("href", response.response.doc[i].web_url).text(response.response.doc[i].web_url);
+            articleDiv.append(headlineel, abstractel, linkel);
+            $("#listOfArticles").append(articleDiv);
+        }
             
-            for(var i=0; i<recordsDisplayed.length; i++){
-                var articleDiv= $("<div>");
-    
-                articleDiv.html(JSON.stringify(response.docs[i]));
-                $("#listOfArticles").append(articleDiv);
-            }
-        });
-    })
+
+        });      
+    });
 });
